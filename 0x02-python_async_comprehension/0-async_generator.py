d64811d51017d1aebb27570c5c0fccddd245b32b
Willmuseve <willmuseve@gmail.com>
2024-03-19 18:43:51 +0000

    Async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A coroutine that takes no arguments
+Random Module is used."""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Coroutine that loosp 10 times
+    each time asynchronous wait 1 second then yield a 
+    random number btn 0 and 10.
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
